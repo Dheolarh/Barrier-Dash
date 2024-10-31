6160931ec20602c60e5ebd0bc7cf05780bba7b15
Dheolarh <adejuwonlookanalwon@gmail.com>
2024-10-31 12:26:44 +0100

    Gravitational Force Modification

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRB;
+    public int jumpforce = 100;
+    public float gravityModifier;
+
     // Start is called before the first frame update
     void Start()
     {
-        int jumpforce = 100;
         _playerRB = GetComponent<Rigidbody>();
-        _playerRB.AddForce(Vector3.up * Time.deltaTime * jumpforce);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRB.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
     }
 }
